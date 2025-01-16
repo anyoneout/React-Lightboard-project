72985c82249ac7c1c6c30bd5136e3fdb2b004db7
anyoneout <chrisdafur@gmail.com>
2025-01-15 18:40:23 -0600

    initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import { createRoot } from "react-dom/client";
+import React from "react";
+
+
+const root = createRoot(window.bodyTag);
+root.render(
+  <>
+    <main></main >
+  </>
+)
+
---
+++ src/utils/output.js
@@ -0,0 +1,4 @@
+export function output(message = "", outputTag = "outputTag", shouldAppend = true) {
+  if (shouldAppend) window[outputTag].innerHTML += message;
+  else window[outputTag].innerHTML = message;
+}; 
